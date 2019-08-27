8336d5c3b11a24e8fe71228772e358fa73ebfc37
Fadrik Alexander <fadi.a.ashy@gmail.com>
2019-08-27 13:11:38 +0300

    The Menu Jam ver 1.0 | fully commented scripts

---
+++ MainMenuController.cs
@@ -0,0 +1,69 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+//This Class will be called from buttons and keys
+public class MainMenuController : MonoBehaviour
+{
+	//Change the Keys in the inspector 
+    [SerializeField]
+    KeyCode QuitKey = KeyCode.Escape;
+
+    [SerializeField]
+    KeyCode PauseKey = KeyCode.P;
+	//an indicator if the game is paused or not
+    bool GamePaused = false;
+
+    void Update()
+    {
+        if (Input.GetKeyDown(QuitKey))
+            Quit();
+
+        if (Input.GetKeyDown(PauseKey))
+            if(!GamePaused)
+                PauseGame();
+            else
+                UnPauseGame();
+    }
+
+	//use this to move between scenes
+	//Called from Buttons
+    public void GoToScene(int SceneNumber)
+    {
+        SceneManager.LoadScene(SceneNumber);
+    }
+
+	//use this to quit the game called from Buttons
+    public void Quit()
+    {
+        Application.Quit();
+    }
+
+	//use this to surf the net
+	//Add the URL as a string in the Button Call
+    public void GoToUrl(string URL)
+    {
+		//when opening while playing WebGl we have to open a new tab in the browser
+        if (Application.platform == RuntimePlatform.WebGLPlayer)
+            Application.ExternalEval("window.open(\"" + URL + " \",\"_blank\")");
+        else
+            Application.OpenURL(URL);
+    }
+	
+	//use this to Pause the Game you can call it from a key or from a button
+    public void PauseGame()
+    {
+        Debug.Log("Pause");
+        GamePaused = true;
+        Time.timeScale = 0;
+    }
+
+	//use this to unPause the Game you can call it from a key or from a button
+    public void UnPauseGame()
+    {
+        Debug.Log("UnPause");
+        GamePaused = false;
+        Time.timeScale = 1;
+    }
+}
---
+++ SoundController.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+//The Script which controls the sounds mechanics
+public class SoundController : MonoBehaviour
+{
+	//Change the Mute Key in the inspector 
+    [SerializeField]
+    KeyCode MuteKey = KeyCode.M;
+
+    AudioSource[] AllAudioSources;
+
+    void Awake()
+    {
+		//Finding all the AudioSource in the scene at the start of the game
+        AllAudioSources = FindObjectsOfType<AudioSource>();
+    }
+
+    void Update()
+    {
+        if (Input.GetKeyDown(MuteKey))
+            MuteGame();
+    }
+
+	//The Mute Function can be activated by pressing the muteKey or by a UI button
+    public void MuteGame()
+    {
+		//Go Through all AudioSources and mute them
+        foreach (AudioSource AS in AllAudioSources)
+        {
+            AS.mute = !AS.mute;
+        }
+    }
+
+	//Change the sound Function can be called by buttons and Sliders to change the volume to all AudioSource
+    public void ChangeSound(Slider slider)
+    {
+		//Go Through all AudioSources and change the volume
+        foreach (AudioSource AS in AllAudioSources)
+        {
+            AS.volume = slider.value;
+        }
+    }
+	
+	//Override
+    public void ChangeSound(float volume)
+    {
+		//Go Through all AudioSources and change the volume
+        foreach (AudioSource AS in AllAudioSources)
+        {
+            AS.volume = volume;
+        }
+    }
+}
